182d31fe9a82cae3e5f892cdead2e8cd004cac3a
reini <reinhold.schachner@hotmail.com>
2021-01-15 14:55:29 +0100

    Dynamically create weather display

--- src/display.js
+++ src/display.js
@@ -4,25 +4,209 @@ function deleteDOM() {
   }
 }
 
-function populateDOM(weatherData) {
+function populateHeader(weatherData) {
+  const header = document.createElement("header");
+
+  const flag = document.createElement("img");
+  //TODO: dynamically change src and alt
+  flag.src = ``;
+  flag.setAttribute("alt", `flag`);
+
   const weatherDescr = document.createElement("p");
+  weatherDescr.classList.add("weather-descr");
   weatherDescr.textContent = weatherData.weather;
 
   const location = document.createElement("p");
+  location.classList.add("weather-location");
   location.textContent = `${weatherData.location}, ${weatherData.country}`;
 
+  header.appendChild(flag);
+  header.appendChild(weatherDescr);
+  header.appendChild(location);
+
+  return header;
+}
+
+function populateMain(weatherData) {
+  const main = document.createElement("main");
+  main.classList.add("grid-container");
+
+  // First column
+  const col1 = document.createElement("div");
+  col1.classList.add("col-6");
+
   const temperature = document.createElement("p");
-  temperature.textContent = `${weatherData.temperature}° C`;
+  temperature.classList.add("big");
+  temperature.textContent = weatherData.temperature;
+
+  const unit = document.createElement("span");
+  unit.classList.add("temp-unit");
+  unit.textContent = "° C";
+
+  temperature.appendChild(unit);
+
+  const tempMin = document.createElement("p");
+  tempMin.classList.add("text-secondary");
+  tempMin.textContent = `min: ${weatherData.tempMin}° C`;
+
+  const tempMax = document.createElement("p");
+  tempMax.classList.add("text-secondary");
+  tempMax.textContent = `max: ${weatherData.tempMax}° C`;
+
+  col1.appendChild(temperature);
+  col1.appendChild(tempMin);
+  col1.appendChild(tempMax);
+
+  // Second column
+  const col2 = document.createElement("div");
+  col2.classList.add("col-6");
 
   // icons see: https://openweathermap.org/weather-conditions#How-to-get-icon-URL
   const icon = document.createElement("img");
   icon.src = `http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
   icon.setAttribute("alt", `${weatherData.weather}`);
 
-  weatherDisplay.appendChild(weatherDescr);
-  weatherDisplay.appendChild(location);
-  weatherDisplay.appendChild(temperature);
-  weatherDisplay.appendChild(icon);
+  // add in a Slider to change from Celsius to Fahrenheit
+
+  col2.appendChild(icon);
+
+  main.appendChild(col1);
+  main.appendChild(col2);
+
+  return main;
+}
+
+function populateContainer(weatherData) {
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  const header = populateHeader(weatherData);
+  const main = populateMain(weatherData);
+
+  container.appendChild(header);
+  container.appendChild(main);
+
+  return container;
+}
+
+function populateFooter(weatherData) {
+  /*
+<footer class="grid-container">
+          <div class="col-6">
+            <div>
+              <p class="text-secondary">Feels like:</p>
+              <p>25° C</p>
+            </div>
+            <div>
+              <p class="text-secondary">Humidity:</p>
+              <p>29%</p>
+            </div>
+            <div>
+              <p class="text-secondary">Wind:</p>
+              <p>5 km/h</p>
+            </div>
+          </div>
+          <div class="col-6">
+            <div>
+              <p class="text-secondary">sunrise</p>
+              <p>7:40</p>
+            </div>
+            <div>
+              <p class="text-secondary">sunset</p>
+              <p>16:50</p>
+            </div>
+            <div>
+              <p class="text-secondary">local time</p>
+              <p>12:30</p>
+            </div>
+          </div>
+        </footer>
+    */
+  const footer = document.createElement("footer");
+  footer.classList.add("grid-container");
+
+  // Column 1
+  const col1 = document.createElement("div");
+  col1.classList.add("col-6");
+
+  const feeling = document.createElement("div");
+  const feelingDescr = document.createElement("p");
+  feelingDescr.classList.add("text-secondary");
+  feelingDescr.textContent = "Feels like";
+  const feelingData = document.createElement("p");
+  feelingData.textContent = `${weatherData.feelsLike}° C`;
+  feeling.appendChild(feelingDescr);
+  feeling.appendChild(feelingData);
+
+  const humidity = document.createElement("div");
+  const humidityDescr = document.createElement("p");
+  humidityDescr.classList.add("text-secondary");
+  humidityDescr.textContent = "Humidity";
+  const humidityData = document.createElement("p");
+  humidityData.textContent = `${weatherData.humidity}%`;
+  humidity.appendChild(humidityDescr);
+  humidity.appendChild(humidityData);
+
+  const wind = document.createElement("div");
+  const windDescr = document.createElement("p");
+  windDescr.classList.add("text-secondary");
+  windDescr.textContent = "Wind";
+  const windData = document.createElement("p");
+  windData.textContent = `${weatherData.wind} km/h`;
+  wind.appendChild(windDescr);
+  wind.appendChild(windData);
+
+  col1.appendChild(feeling);
+  col1.appendChild(humidity);
+  col1.appendChild(wind);
+
+  // Column 2
+  const col2 = document.createElement("div");
+  col2.classList.add("col-6");
+
+  const sunrise = document.createElement("div");
+  const sunriseDescr = document.createElement("p");
+  sunriseDescr.classList.add("text-secondary");
+  sunriseDescr.textContent = "Sunrise";
+  const sunriseData = document.createElement("p");
+  sunriseData.textContent = weatherData.sunrise;
+  sunrise.appendChild(sunriseDescr);
+  sunrise.appendChild(sunriseData);
+
+  const sunset = document.createElement("div");
+  const sunsetDescr = document.createElement("p");
+  sunsetDescr.classList.add("text-secondary");
+  sunsetDescr.textContent = "Sunset";
+  const sunsetData = document.createElement("p");
+  sunsetData.textContent = weatherData.sunset;
+  sunset.appendChild(sunsetDescr);
+  sunset.appendChild(sunsetData);
+
+  const localTime = document.createElement("div");
+  const localTimeDescr = document.createElement("p");
+  localTimeDescr.classList.add("text-secondary");
+  localTimeDescr.textContent = "Local time";
+  const localTimeData = document.createElement("p");
+  localTimeData.textContent = weatherData.timezone;
+  localTime.appendChild(localTimeDescr);
+  localTime.appendChild(localTimeData);
+
+  col2.appendChild(sunrise);
+  col2.appendChild(sunset);
+  col2.appendChild(localTime);
+
+  footer.appendChild(col1);
+  footer.appendChild(col2);
+
+  return footer;
+}
+
+function populateDOM(weatherData) {
+  const container = populateContainer(weatherData);
+  const footer = populateFooter(weatherData);
+
+  weatherDisplay.appendChild(container);
+  weatherDisplay.appendChild(footer);
 }
 
 const weatherDisplay = document.querySelector(".weather-display");
--- src/index.js
+++ src/index.js
@@ -14,14 +14,41 @@ async function createWeatherObject(location) {
   const data = await getWeather(location);
   // log data for reference for now
   console.log(data);
+
+  // helpers for dates
+  function getTime(secs) {
+    // openweathermap returns seconds, so they are multiplied with 1000 to get the milliseconds required to construct the correct date
+    const date = new Date(secs * 1000);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours}:${minutes}`;
+  }
+
+  function localTime(offset) {
+    const date = new Date();
+    let hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+
+    const hourOffset = (offset / 3600).toFixed(0);
+    hours += Number(hourOffset);
+    if (hours >= 24) hours -= 24;
+
+    return `${hours}:${minutes}`;
+  }
   return {
     location: data.name,
     country: data.sys.country,
-    temperature: data.main.temp,
+    temperature: data.main.temp.toFixed(1),
+    tempMin: data.main.temp_min.toFixed(1),
+    tempMax: data.main.temp_max.toFixed(1),
+    feelsLike: data.main.feels_like.toFixed(1),
+    humidity: data.main.humidity,
     weather: data.weather[0].description,
     icon: data.weather[0].icon,
-    wind: data.wind.speed,
-    cloudiness: data.clouds.all,
+    wind: (data.wind.speed * 3.6).toFixed(1), //default is meter/second. Multipy with 3.6 to convert in km/h
+    sunrise: getTime(data.sys.sunrise),
+    sunset: getTime(data.sys.sunset),
+    timezone: localTime(data.timezone), //this is the shift from UTC in seconds. Calculate local time base on this
   };
 }
 
