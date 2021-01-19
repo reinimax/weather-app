a8a4ce510beaace05829fac1d955481e0fb17ae3
reini <reinhold.schachner@hotmail.com>
2021-01-19 18:14:59 +0100

    Add error handling

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import * as display from "./display";
+const errorInfo = document.querySelector(".error-info");
 
 async function getWeather(location) {
-  // TODO: add error-catching!
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=04677966a703b0124f576651b3349deb&units=metric`
-  );
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=04677966a703b0124f576651b3349deb&units=metric`
+    );
 
-  const weather = await response.json();
-  return weather;
+    // extra handling of 404, since this will be the most common one
+    if (response.status === 404) {
+      errorInfo.textContent = "Couldn't find this place...";
+    } else if (!response.ok) {
+      errorInfo.textContent = "Something went wrong";
+    } else {
+      const weather = await response.json();
+      return weather;
+    }
+  } catch (err) {
+    // the catch block activates only on network error, NOT when a response with an error-code is received. These cases have to be handled above in the try-block.
+    errorInfo.textContent = err.message;
+  }
 }
 
 async function createWeatherObject(location) {
-  const data = await getWeather(location);
-  // log data for reference for now
-  console.log(data);
-
   // helpers for dates
   function getTime(secs) {
     // openweathermap returns seconds, so they are multiplied with 1000 to get the milliseconds required to construct the correct date
@@ -50,32 +58,41 @@ async function createWeatherObject(location) {
     return `${hours}:${minutes}`;
   }
 
-  return {
-    location: data.name,
-    country: data.sys.country,
-    temperature: data.main.temp.toFixed(1),
-    tempMin: data.main.temp_min.toFixed(1),
-    tempMax: data.main.temp_max.toFixed(1),
-    feelsLike: data.main.feels_like.toFixed(1),
-    humidity: data.main.humidity,
-    weather: data.weather[0].description,
-    icon: data.weather[0].icon,
-    wind: (data.wind.speed * 3.6).toFixed(1), //default is meter/second. Multipy with 3.6 to convert in km/h
-    sunrise: getTime(data.sys.sunrise),
-    sunset: getTime(data.sys.sunset),
-    timezone: localTime(data.timezone), //this is the shift from UTC in seconds. Calculate local time base on this
-  };
+  // create an Object with the relevant data
+  try {
+    const data = await getWeather(location);
+
+    return {
+      location: data.name,
+      country: data.sys.country,
+      temperature: data.main.temp.toFixed(1),
+      tempMin: data.main.temp_min.toFixed(1),
+      tempMax: data.main.temp_max.toFixed(1),
+      feelsLike: data.main.feels_like.toFixed(1),
+      humidity: data.main.humidity,
+      weather: data.weather[0].description,
+      icon: data.weather[0].icon,
+      wind: (data.wind.speed * 3.6).toFixed(1), //default is meter/second. Multipy with 3.6 to convert in km/h
+      sunrise: getTime(data.sys.sunrise),
+      sunset: getTime(data.sys.sunset),
+      timezone: localTime(data.timezone), //this is the shift from UTC in seconds. Calculate local time base on this
+    };
+  } catch (err) {}
 }
 
 async function displayWeather(e) {
   e.preventDefault(); // prevent form from reloading the page
   const location = e.target.querySelector("input[type='search']").value;
 
-  const weatherObject = await createWeatherObject(location);
-  console.log(weatherObject);
+  try {
+    const weatherObject = await createWeatherObject(location);
 
-  display.deleteDOM();
-  display.populateDOM(weatherObject);
+    display.deleteDOM();
+    display.populateDOM(weatherObject);
+
+    //reset error-message
+    errorInfo.textContent = "";
+  } catch (err) {}
 }
 
 const search = document.querySelector("#weather-search");
